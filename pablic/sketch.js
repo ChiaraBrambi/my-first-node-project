68adb298d120df4105b3a54bfa4ac08b25855025
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-20 18:16:44 +0100

    base

--- pablic/sketch.js
+++ pablic/sketch.js
@@ -1,9 +1,16 @@
 let socket = io();
+let myColor = 'white';
+
 socket.on("connect", newConnection);
 socket.on("mouseBroadcast", drawOtherMouse);
+socket.on("color", setColor);
 
 function newConnection(){
   console.log("your id"+ socket.id);
+}
+
+function setColor(assignedColor){
+  myColor = assignedColor;
 }
 
 function drawOtherMouse(data){
@@ -22,9 +29,11 @@ function draw() {}
 
 //connettersi a localhost:3000 per vedere. lo sketch scrivendo node.js nel prompt comandi
 function mouseMoved(){
+  push();
   fill('orange');
   noStroke();
   ellipse(mouseX, mouseY,20);
+  pop();
   //create the message
   let message ={
     x: mouseX,
