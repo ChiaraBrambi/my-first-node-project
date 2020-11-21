b833c0444004dda293ab2acf215e98045084a8d9
chiara <chiara7.brambilla@mail.polimi.it>
2020-11-21 12:29:53 +0100

    restore

--- pablic/sketch.js
+++ pablic/sketch.js
@@ -1,44 +1,82 @@
 let socket = io();
 let myColor = 'white';
-
+//quando arriva il messaggio attiva la funzione
 socket.on("connect", newConnection);
-socket.on("mouseBroadcast", drawOtherMouse);
+socket.on("mouseBroadcast", otherMouse);
 socket.on("color", setColor);
-
-function newConnection(){
-  console.log("your id"+ socket.id);
-}
+socket.on('newPlayer', newPlayer);
 
 function setColor(assignedColor){
   myColor = assignedColor;
-}
+  }
 
-function drawOtherMouse(data){
-  fill('pink');
-  noStroke();
-  ellipse(data.x, data.y,20);
-}
+  function newConnection() {
+    console.log("your id:", socket.id);
+  }
 
+  //quali info vuoi mandare tra i client?
+  function otherMouse(data){
+    push();
+    fill(data.color);
+    ellipse(data.x, data.y,20);
+    pop();
+  }
+
+  function newPlayer(newPlayerColor){
+    console.log(newPlayerColor);
+    push();
+    fill('purple');
+    rectMode(CENTER);
+    rect(width/2,height/2,300, 100);
+    textAlign('center');
+    fill(newPlayerColor);
+    textSize(20);
+    text('new player joined: ' + newPlayerColor, width/2, height/2);
+    pop();
+  }
+
+
+var cnv;
 function preload(){}
 
+function centerCanvas() {
+var q = (windowWidth - width) / 2;
+var s = (windowHeight - height) / 2;
+cnv.position(q, s);
+}
+
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  background('purple');}
+    cnv = createCanvas(windowHeight, windowHeight);
+    centerCanvas();
+    ellipseMode(CORNER);
+    frameRate(12);
+    push();
+    background("purple");
+    textSize(30);
+    textAlign('center');
+    fill(myColor);
+    text('Walcame'+ myColor, width/2,height/2);
+    pop();
+}
 
-function draw() {}
 
-//connettersi a localhost:3000 per vedere. lo sketch scrivendo node.js nel prompt comandi
 function mouseMoved(){
-  push();
-  fill('orange');
-  noStroke();
-  ellipse(mouseX, mouseY,20);
-  pop();
-  //create the message
+  fill(myColor);
+  ellipse(mouseX,mouseY,20);
+  text('me', mouseX,mouseY);
   let message ={
     x: mouseX,
     y: mouseY,
-  };
-  //send to the server
-  socket.emit("mouse", message);
+    color: myColor,
+  }
+//sand to the server
+socket.emit("mouse", message);
+}
+
+function draw() {
+}
+
+
+function windowResized() {
+  centerCanvas();
 }
--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-//servere file
-console.log("node is running");
-//we will create a local lost by the instruction on our compute
-//pakege node express
-let express = require("express");//loded all the code inside a variable
-let socket = require("socket.io");
+console.log("node is running")
+//load expression
+let express = require("express");
+//web app, local host
 let app = express();
 let port = 3000;
 let server = app.listen(port);
-app.use(express.static('pablic'));
-//create web socket : client and server discussion, this in comand prompt
-
-//io = input-output
+app.use(express.static("public"));
+// carica libreria socket attraverso command promt
+let socket = require("socket.io");
+// una nuova var create a socket connection
 let io = socket(server);
-io.on("connection", newConnection);//name of the function that have to be executed
 
-function newConnection(socket){
-  console.log("new connection:" + socket.client.id);
+io.on("connection", newConnection);
 
-  socket.on("mouse", mouseMessage);
+function newConnection(socket){//funzione che si attiva ogni volta che c'è una nuova connessione
+console.log("new connection: " + socket.client.id);
 
-  function mouseMessage(dataRecived){
-    console.log(socket.client.id, dataRecived);
-    socket.broadcast.emit("mouseBroadcast", dataRecived);
+
+let clientColor= getRandomColor();
+socket.emit('color',clientColor);
+
+socket.broadcast.emit('newPlayer', clientColor);
+
+socket.on("mouse",mouseMessage);
+function mouseMessage(dataRecived){
+  console.log(socket.client.id, dataRecived);//lo visualizzo nel commondo prompt
+  socket.broadcast.emit("mouseBroadcast", dataRecived);//lo manda a tutti i client tranne a me
   }
 }
 
-unction getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i=0; i<6; i++){
+    color+= letters[Math.floor(Math.random()*16)];
+    }
+    return color;
 }
